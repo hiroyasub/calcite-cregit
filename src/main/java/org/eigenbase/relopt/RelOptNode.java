3e41421b9ef66635ef43b6a2233ab3e3cb874f6e
Julian Hyde <julianhyde@gmail.com>
2012-05-07 14:11:08 -0700

    Oops!
    Former-commit-id: 99785f20ca32248578829c10445faa4dc7d21a2e

--- src/main/java/org/eigenbase/relopt/RelOptNode.java
+++ src/main/java/org/eigenbase/relopt/RelOptNode.java
@@ -1,4 +1,8 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/* // Licensed to Julian Hyde under one or more contributor license // agreements. See the NOTICE file distributed with this work for // additional information regarding copyright ownership. // // Julian Hyde licenses this file to you under the Apache License, // Version 2.0 (the "License"); you may not use this file except in // compliance with the License. You may obtain a copy of the License at: // // http://www.apache.org/licenses/LICENSE-2.0 // // Unless required by applicable law or agreed to in writing, software // distributed under the License is distributed on an "AS IS" BASIS, // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. // See the License for the specific language governing permissions and // limitations under the License. */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -9,99 +13,63 @@ name|relopt
 package|;
 end_package
 
-begin_comment
-comment|// Licensed to Julian Hyde under one or more contributor license
-end_comment
+begin_import
+import|import
+name|org
+operator|.
+name|eigenbase
+operator|.
+name|reltype
+operator|.
+name|RelDataType
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|List
+import|;
+end_import
 
 begin_comment
-comment|// agreements. See the NOTICE file distributed with this work for
+comment|/**  * Node in a planner.  */
 end_comment
 
-begin_comment
-comment|// additional information regarding copyright ownership.
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|// Julian Hyde licenses this file to you under the Apache License,
-end_comment
-
-begin_comment
-comment|// Version 2.0 (the "License"); you may not use this file except in
-end_comment
-
-begin_comment
-comment|// compliance with the License. You may obtain a copy of the License at:
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|// http://www.apache.org/licenses/LICENSE-2.0
-end_comment
-
-begin_comment
-comment|//
-end_comment
-
-begin_comment
-comment|// Unless required by applicable law or agreed to in writing, software
-end_comment
-
-begin_comment
-comment|// distributed under the License is distributed on an "AS IS" BASIS,
-end_comment
-
-begin_comment
-comment|// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-end_comment
-
-begin_comment
-comment|// See the License for the specific language governing permissions and
-end_comment
-
-begin_comment
-comment|// limitations under the License.
-end_comment
-
-begin_expr_stmt
-operator|*
-operator|/
+begin_interface
 specifier|public
-expr|interface
+interface|interface
 name|RelOptNode
 block|{
 comment|/**      * Returns the ID of this relational expression, unique among all relational      * expressions created since the server was started.      *      * @return Unique ID      */
 name|int
 name|getId
-argument_list|()
-block|;
+parameter_list|()
+function_decl|;
 comment|/**      * Returns a string which concisely describes the definition of this      * relational expression. Two relational expressions are equivalent if and      * only if their digests are the same.      *      *<p>The digest does not contain the relational expression's identity --      * that would prevent similar relational expressions from ever comparing      * equal -- but does include the identity of children (on the assumption      * that children have already been normalized).      *      *<p>If you want a descriptive string which contains the identity, call      * {@link Object#toString()}, which always returns "rel#{id}:{digest}".      */
 name|String
 name|getDigest
-argument_list|()
-block|;
+parameter_list|()
+function_decl|;
 comment|/**      * Retrieves this RelNode's traits. Note that although the RelTraitSet      * returned is modifiable, it<b>must not</b> be modified during      * optimization. It is legal to modify the traits of a RelNode before or      * after optimization, although doing so could render a tree of RelNodes      * unimplementable. If a RelNode's traits need to be modified during      * optimization, clone the RelNode and change the clone's traits.      *      * @return this RelNode's trait set      */
 name|RelTraitSet
 name|getTraitSet
-argument_list|()
-block|;
+parameter_list|()
+function_decl|;
 comment|// TODO: We don't want to require that nodes have very detailed row type. It
 comment|// may not even be known at planning time.
 name|RelDataType
 name|getRowType
-argument_list|()
-block|;
+parameter_list|()
+function_decl|;
 comment|/**      * Returns a string which describes the relational expression and, unlike      * {@link #getDigest()}, also includes the identity. Typically returns      * "rel#{id}:{digest}".      */
 name|String
 name|getDescription
-argument_list|()
-block|;
+parameter_list|()
+function_decl|;
 name|List
 argument_list|<
 name|?
@@ -109,14 +77,15 @@ extends|extends
 name|RelOptNode
 argument_list|>
 name|getInputs
-argument_list|()
-block|;
+parameter_list|()
+function_decl|;
 comment|/**      * Returns the cluster this relational expression belongs to.      *      * @return cluster      */
 name|RelOptCluster
 name|getCluster
-argument_list|()
-block|; }
-end_expr_stmt
+parameter_list|()
+function_decl|;
+block|}
+end_interface
 
 begin_comment
 comment|// End RelNode.java
