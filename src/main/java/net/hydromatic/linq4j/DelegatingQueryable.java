0bb697a18d954bac68be6d65c0c4df954d842c98
Julian Hyde <julianhyde@gmail.com>
2012-10-12 18:10:51 -0700

    BlockBuilder.append(Expression) now makes expression into a return statement, just as .add(Expression) does.
    Former-commit-id: 4ca0a824a57bb81f12fa73fcf77405151bf7fc9a

---
+++ src/main/java/net/hydromatic/linq4j/DelegatingQueryable.java
@@ -0,0 +1,24 @@
+begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|net
+operator|.
+name|hydromatic
+operator|.
+name|linq4j
+package|;
+end_package
+
+begin_comment
+comment|/**  * Created with IntelliJ IDEA.  * User: jhyde  * Date: 7/25/12  * Time: 2:54 PM  * To change this template use File | Settings | File Templates.  */
+end_comment
+
+begin_class
+specifier|public
+class|class
+name|DelegatingQueryable
+block|{ }
+end_class
+
+end_unit
+
