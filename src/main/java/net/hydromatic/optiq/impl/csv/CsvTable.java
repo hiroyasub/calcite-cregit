38f5eeea9afb52e8ad6c17a4864a705539ed95a1
Julian Hyde <julianhyde@gmail.com>
2013-10-14 17:52:25 -0700

    Upgrade to optiq-0.4.15.
    Former-commit-id: 5b0e70c7457d23f7cf5ddb74c78395f5ad45476c

--- src/main/java/net/hydromatic/optiq/impl/csv/CsvTable.java
+++ src/main/java/net/hydromatic/optiq/impl/csv/CsvTable.java
@@ -328,15 +328,6 @@ argument_list|()
 return|;
 block|}
 specifier|public
-name|DataContext
-name|getDataContext
-parameter_list|()
-block|{
-return|return
-name|schema
-return|;
-block|}
-specifier|public
 name|Class
 name|getElementType
 parameter_list|()
