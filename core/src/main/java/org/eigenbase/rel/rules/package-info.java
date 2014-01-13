1ac1c60ced61beef743c84b4a9ed443541c9f651
julianhyde <julianhyde@gmail.com>
2014-01-13 14:07:15 -0800

    Rename "package.html" files to "package-info.java".
    Former-commit-id: 437371ac4c6e93b6c56fed5b2a97c8b55a29f79d

---
+++ core/src/main/java/org/eigenbase/rel/rules/package-info.java
@@ -0,0 +1,27 @@
+begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/* // Licensed to Julian Hyde under one or more contributor license // agreements. See the NOTICE file distributed with this work for // additional information regarding copyright ownership. // // Julian Hyde licenses this file to you under the Apache License, // Version 2.0 (the "License"); you may not use this file except in // compliance with the License. You may obtain a copy of the License at: // // http://www.apache.org/licenses/LICENSE-2.0 // // Unless required by applicable law or agreed to in writing, software // distributed under the License is distributed on an "AS IS" BASIS, // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. // See the License for the specific language governing permissions and // limitations under the License. */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|eigenbase
+operator|.
+name|rel
+operator|.
+name|rules
+package|;
+end_package
+
+begin_comment
+comment|/**  * Defines relational expressions.<p>&nbsp;</p>  *  *<h2>Related packages and classes</h2>  *<ul>  *<li>Package<code><a href="../sql/package-summary.html">org.eigenbase.sql</a></code>  *        is an object model for SQL expressions</li>  *<li>Package<code><a href="../sql/package-summary.html">org.eigenbase.rex</a></code>  *        is an object model for relational row expressions</li>  *<li>Package<code><a href="../relopt/package-summary.html">org.eigenbase.relopt</a></code>  *        provides an  *        optimizer interface.</li>  *</ul> */
+end_comment
+
+begin_comment
+comment|// End package-info.java
+end_comment
+
+end_unit
+
