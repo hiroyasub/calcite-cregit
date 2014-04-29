d45a75a7cfcc43448ef2557d83f58ba396b86193
vkorukanti <venki.korukanti@gmail.com>
2014-04-29 04:53:32 -0700

    [OPTIQ-324] Add ViewExpander for Planner in Frameworks. Expose additional properties of ViewTable to allow subclassing.
    Former-commit-id: ff40b4e095420add9b44c228958f51b4b00eb756

--- core/src/main/java/net/hydromatic/optiq/impl/ViewTable.java
+++ core/src/main/java/net/hydromatic/optiq/impl/ViewTable.java
@@ -682,6 +682,29 @@ argument_list|)
 return|;
 block|}
 block|}
+comment|/** Get the view's SQL definition. */
+specifier|public
+name|String
+name|getViewSql
+parameter_list|()
+block|{
+return|return
+name|viewSql
+return|;
+block|}
+comment|/** Get the schemapath of the view */
+specifier|public
+name|List
+argument_list|<
+name|String
+argument_list|>
+name|getSchemaPath
+parameter_list|()
+block|{
+return|return
+name|schemaPath
+return|;
+block|}
 block|}
 end_class
 
