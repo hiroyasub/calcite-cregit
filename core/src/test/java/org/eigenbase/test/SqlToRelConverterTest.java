16548bc33cbdf34f95f108e18b476bce13e97000
Julian Hyde <jhyde@apache.org>
2014-07-31 16:07:42 -0700

    Change license headers from "Julian Hyde" to "ASF"; add headers where missing.
    Former-commit-id: bfa3daeeadd4926c6bf5af2b507ba5065359d47f

--- core/src/test/java/org/eigenbase/test/SqlToRelConverterTest.java
+++ core/src/test/java/org/eigenbase/test/SqlToRelConverterTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:1.0.0;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/* // Licensed to Julian Hyde under one or more contributor license // agreements. See the NOTICE file distributed with this work for // additional information regarding copyright ownership. // // Julian Hyde licenses this file to you under the Apache License, // Version 2.0 (the "License"); you may not use this file except in // compliance with the License. You may obtain a copy of the License at: // // http://www.apache.org/licenses/LICENSE-2.0 // // Unless required by applicable law or agreed to in writing, software // distributed under the License is distributed on an "AS IS" BASIS, // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. // See the License for the specific language governing permissions and // limitations under the License. */
+comment|/* // Licensed to the Apache Software Foundation (ASF) under one or more // contributor license agreements.  See the NOTICE file distributed with // this work for additional information regarding copyright ownership. // The ASF licenses this file to you under the Apache License, Version 2.0 // (the "License"); you may not use this file except in compliance with // the License.  You may obtain a copy of the License at // // http://www.apache.org/licenses/LICENSE-2.0 // // Unless required by applicable law or agreed to in writing, software // distributed under the License is distributed on an "AS IS" BASIS, // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. // See the License for the specific language governing permissions and // limitations under the License. */
 end_comment
 
 begin_package
@@ -1799,7 +1799,7 @@ name|check
 argument_list|(
 literal|"select deptno from emp as emp0 cross join\n"
 operator|+
-literal|" (select empno from emp union all \n"
+literal|" (select empno from emp union all\n"
 operator|+
 literal|"  select deptno from dept where deptno> 20 union all\n"
 operator|+
