ef80d80eea3bcc4afe504505985d89f26225f7f9
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2014-12-14 23:56:52 +0300

    Minor cleanup in JdbcTest#testCloneGroupBy2
    Former-commit-id: fede7e568398370ec73e8061a63c2f07f58c39d2

--- core/src/test/java/org/apache/calcite/test/JdbcTest.java
+++ core/src/test/java/org/apache/calcite/test/JdbcTest.java
@@ -6267,6 +6267,9 @@ expr_stmt|;
 block|}
 annotation|@
 name|Ignore
+argument_list|(
+literal|"The test returns expected results. Not sure why it is disabled"
+argument_list|)
 annotation|@
 name|Test
 specifier|public
@@ -6293,37 +6296,40 @@ argument_list|(
 literal|"select \"time_by_day\".\"the_year\" as \"c0\", \"time_by_day\".\"quarter\" as \"c1\", \"product_class\".\"product_family\" as \"c2\", sum(\"sales_fact_1997\".\"unit_sales\") as \"m0\" from \"time_by_day\" as \"time_by_day\", \"sales_fact_1997\" as \"sales_fact_1997\", \"product_class\" as \"product_class\", \"product\" as \"product\" where \"sales_fact_1997\".\"time_id\" = \"time_by_day\".\"time_id\" and \"time_by_day\".\"the_year\" = 1997 and \"sales_fact_1997\".\"product_id\" = \"product\".\"product_id\" and \"product\".\"product_class_id\" = \"product_class\".\"product_class_id\" group by \"time_by_day\".\"the_year\", \"time_by_day\".\"quarter\", \"product_class\".\"product_family\""
 argument_list|)
 operator|.
-name|returns
+name|returnsUnordered
 argument_list|(
-literal|"c0=1997; c1=Q2; c2=Drink; m0=5895.0000\n"
-operator|+
-literal|"c0=1997; c1=Q1; c2=Food; m0=47809.0000\n"
-operator|+
-literal|"c0=1997; c1=Q3; c2=Drink; m0=6065.0000\n"
-operator|+
-literal|"c0=1997; c1=Q4; c2=Drink; m0=6661.0000\n"
-operator|+
-literal|"c0=1997; c1=Q4; c2=Food; m0=51866.0000\n"
-operator|+
-literal|"c0=1997; c1=Q1; c2=Drink; m0=5976.0000\n"
-operator|+
-literal|"c0=1997; c1=Q3; c2=Non-Consumable; m0=12343.0000\n"
-operator|+
-literal|"c0=1997; c1=Q4; c2=Non-Consumable; m0=13497.0000\n"
-operator|+
-literal|"c0=1997; c1=Q2; c2=Non-Consumable; m0=11890.0000\n"
-operator|+
-literal|"c0=1997; c1=Q2; c2=Food; m0=44825.0000\n"
-operator|+
-literal|"c0=1997; c1=Q3; c2=Food; m0=47440.0000\n"
-operator|+
-literal|"c0=1997; c1=Q1; c2=Non-Consumable; m0=12506.0000\n"
+literal|"c0=1997; c1=Q2; c2=Drink; m0=5895.0000"
+argument_list|,
+literal|"c0=1997; c1=Q1; c2=Food; m0=47809.0000"
+argument_list|,
+literal|"c0=1997; c1=Q3; c2=Drink; m0=6065.0000"
+argument_list|,
+literal|"c0=1997; c1=Q4; c2=Drink; m0=6661.0000"
+argument_list|,
+literal|"c0=1997; c1=Q4; c2=Food; m0=51866.0000"
+argument_list|,
+literal|"c0=1997; c1=Q1; c2=Drink; m0=5976.0000"
+argument_list|,
+literal|"c0=1997; c1=Q3; c2=Non-Consumable; m0=12343.0000"
+argument_list|,
+literal|"c0=1997; c1=Q4; c2=Non-Consumable; m0=13497.0000"
+argument_list|,
+literal|"c0=1997; c1=Q2; c2=Non-Consumable; m0=11890.0000"
+argument_list|,
+literal|"c0=1997; c1=Q2; c2=Food; m0=44825.0000"
+argument_list|,
+literal|"c0=1997; c1=Q3; c2=Food; m0=47440.0000"
+argument_list|,
+literal|"c0=1997; c1=Q1; c2=Non-Consumable; m0=12506.0000"
 argument_list|)
 expr_stmt|;
 block|}
 comment|/** Tests plan for a query with 4 tables, 3 joins. */
 annotation|@
 name|Ignore
+argument_list|(
+literal|"The actual and expected plan differ"
+argument_list|)
 annotation|@
 name|Test
 specifier|public
