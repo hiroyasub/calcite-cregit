b0708b22ffbc5137e7a09cc088c361d335486ba9
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-03-25 15:06:11 +0300

    [CALCITE-3660] Disable PigRelBuilderStyleTest#testScanAndFilter since it fails too often for no reason
    Former-commit-id: 989fc124976a5e701cce06b2f62988c8ccbb5988

--- pig/src/test/java/org/apache/calcite/test/PigRelBuilderStyleTest.java
+++ pig/src/test/java/org/apache/calcite/test/PigRelBuilderStyleTest.java
@@ -575,6 +575,11 @@ argument_list|)
 expr_stmt|;
 block|}
 annotation|@
+name|Disabled
+argument_list|(
+literal|"CALCITE-3660"
+argument_list|)
+annotation|@
 name|Test
 specifier|public
 name|void
