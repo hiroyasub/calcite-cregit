20d1169e1a46ac2cc1d4834e354814969326fb33
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-09-03 00:12:22 +0300

    [CALCITE-4215] Avoid NPE when monotonicity returns null
    Former-commit-id: 58adcae950666bde29687eab401a0d4b479a2071

--- core/src/main/java/org/apache/calcite/sql/fun/SqlFloorFunction.java
+++ core/src/main/java/org/apache/calcite/sql/fun/SqlFloorFunction.java
@@ -330,13 +330,24 @@ name|call
 parameter_list|)
 block|{
 comment|// Monotonic iff its first argument is, but not strict.
-return|return
+name|SqlMonotonicity
+name|monotonicity
+init|=
 name|call
 operator|.
 name|getOperandMonotonicity
 argument_list|(
 literal|0
 argument_list|)
+decl_stmt|;
+return|return
+name|monotonicity
+operator|==
+literal|null
+condition|?
+literal|null
+else|:
+name|monotonicity
 operator|.
 name|unstrict
 argument_list|()
