14d98868529fbb179cdcc4f7568d0bb1d6d6a728
Julian Hyde <jhyde@apache.org>
2022-04-28 17:29:19 -0700

    Quidem: Allow CREATE VIEW in 'scott' connection
    Former-commit-id: c40aa4ce7e6a3ef6347785a76e0307ea5b2255ec

--- server/src/main/java/org/apache/calcite/server/ServerDdlExecutor.java
+++ server/src/main/java/org/apache/calcite/server/ServerDdlExecutor.java
@@ -1071,6 +1071,22 @@ end_import
 
 begin_import
 import|import
+name|org
+operator|.
+name|checkerframework
+operator|.
+name|checker
+operator|.
+name|nullness
+operator|.
+name|qual
+operator|.
+name|Nullable
+import|;
+end_import
+
+begin_import
+import|import
 name|java
 operator|.
 name|io
@@ -1422,6 +1438,8 @@ specifier|static
 name|SqlNode
 name|renameColumns
 parameter_list|(
+annotation|@
+name|Nullable
 name|SqlNodeList
 name|columnList
 parameter_list|,
